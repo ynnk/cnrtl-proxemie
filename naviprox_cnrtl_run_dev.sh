a12b309e890e06170e10638ea313b9f12ba1f6ea
ynnk <ychudy@gmail.com>
2014-09-16 09:27:32 +0200

    * dev version of gunicorn launcher   no virtualenv dev user

---
+++ naviprox_cnrtl_run_dev.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+## Run CILLEX throw gunicorn
+
+set -x  #log all execed lin for debug
+#set -e
+BASEDIR=/work/cello.cnrtl/
+APPMODULE=naviprox_cnrtl
+APPNAME=app
+
+# log
+LOGFILE=$BASEDIR/log/cnrtl.log 
+LOGDIR=$(dirname $LOGFILE)
+LOGLEVEL=debug
+
+# gunicorn config
+BIND=0.0.0.0:8042
+NUM_WORKERS=1
+
+# user/group to run as
+USER=yk
+GROUP=yk
+
+## start the app
+cd $BASEDIR
+# if  virtualenv is used 
+#source ../bin/activate
+
+
+#pre-start script
+# create log dir if not exist
+test -d $LOGDIR || mkdir -p $LOGDIR
+#end script
+
+# run the gunicorn server
+exec gunicorn --workers $NUM_WORKERS --bind=$BIND\
+    --user=$USER --group=$GROUP --log-level=$LOGLEVEL \
+    --log-file=$LOGFILE $APPMODULE:$APPNAME #2>>$LOGFILE
